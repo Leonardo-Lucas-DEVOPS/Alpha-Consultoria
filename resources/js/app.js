cf1b454b9345bbb99825321456fb4e5d9e24d8ee
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-07-23 20:42:48 -0300

    First commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
+import 'bootstrap'; // Importa o JavaScript do Bootstrap
+
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
