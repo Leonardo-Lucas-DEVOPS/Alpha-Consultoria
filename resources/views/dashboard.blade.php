652a55fd650712df59dfff473b18df044e4491f0
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-07-23 21:36:59 -0300

    - Criação do dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,34 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            olá
-        </h2>
-    </x-slot>
-    
+    <x-app-layout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="container p-6 text-gray-900">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12 mb-4">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Consulta de funcionário</h5>
+                                        <p class="card-text">Serviço de consulta criminal e processual, estadual e municipal, e antecedentes criminais de pessoas.</p>
+                                        <a href="#" class="btn btn-primary">Consultar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12 mb-4">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Consulta de prestadores de serviço</h5>
+                                        <p class="card-text">Consulta criminal e processual, status de CNH e veículo de prestadores.</p>
+                                        <a href="#" class="btn btn-primary">Consultar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+        </div>
+    </x-app-layout>
