d4c56ffe8301f7650f811c08be537fe024f27912
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-07-26 17:48:56 -0300

    - criação das migrate  - criação das model  - ajuste da controller

--- database/migrations/2024_07_26_163756_create_freelancers_table.php
+++ database/migrations/2024_07_26_163756_create_freelancers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('freelancers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('rg');
+            $table->string('cpf')->unique();
+            $table->date('nascimento');
+            $table->string('pai')->nullable();
+            $table->string('mae');
+            $table->string('placa')->unique();
+            $table->string('cnh')->unique();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('return_status')->default('Em análise');
             $table->timestamps();
         });
     }
