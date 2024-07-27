a47d64042cd20885489cf355ae4b01103faaa085
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-07-27 20:53:50 -0300

    - criação do metodo store

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -12,4 +12,33 @@ class VehicleController extends Controller
     public function create(){
         return view('vehicle.create-vehicle');
     }
+    
+    public function store(Request $request){
+        try {
+            
+            $validatedData = $request->validade([
+                'placa'=>'required|unique:vehicles,placa',
+                'chassi'=>'required|unique:vehicles,chassi',
+                'renavam'=>'required|unique:vehicles,renavam',
+                
+            ]); 
+            
+            $validatedData['placa'] = preg_replace('/\D/', '', $validatedData['placa']);
+            $validatedData['cnh'] = preg_replace('/\D/', '', $validatedData['cnh']);
+            $validatedData['placa'] = preg_replace('/\D/', '', $validatedData['placa']);
+
+
+            $userId = Auth::id();
+            Vehicle::create(array_merge($validatedData, ['user_id' => $userId]));
+            
+
+            return redirect(route('dashboard'))->with('success','Registro criado com sucesso');
+        }
+        catch (ValidationException $e) {
+            return redirect(route('dashboard'))->with('fail','Falha na validação dos dados' . $e->getMessage());
+        }
+        catch (ValidationException $e) {
+            return redirect(route('dashboard'))->with('fail','Falha no registros dos dados' . $e->getMessage());
+        }
+    }
 }
