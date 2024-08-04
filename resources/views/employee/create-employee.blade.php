f147a120fa1841877c513c3aba8de33046474566
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-08-04 19:42:38 -0300

    - Reorganização : create-classe contem form para criação de novos registros ou criação de alterações

--- resources/views/employee/create-employee.blade.php
+++ resources/views/employee/create-employee.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    @include('vehicle.partials.vehicle-form')
+                    @include('employee.partials.employee-form')
                 </div>
             </div>
         </div>
