1e7414d5426ffa354f7e29372b1942a78c769b62
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-08-04 19:45:13 -0300

    - Reorganização : todos os raquivos show-classe possuem componentes partials de exibição de listagem de alterações para auditoria e list com todas as consultas cadastradas

--- resources/views/freelancer/show-freelancer.blade.php
+++ resources/views/freelancer/show-freelancer.blade.php
@@ -3,11 +3,11 @@
         <div class="max-w-7x2 mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-x2">
-                    @include('vehicle.partials.vehicle-list')
+                    @include('freelancer.partials.freelancer-list')
                 </div>
             </div>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                @include('vehicle.partials.vehicle-audit')
+                @include('freelancer.partials.freelancer-audit')
             </div>
         </div>
     </div>
