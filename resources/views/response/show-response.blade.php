04b8a8b189eabe14bb81f604b3d20add447f4bc3
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-08-07 23:52:25 -0300

    - criação da view Response  exclusivamente para trabalhar nas consultas

---
+++ resources/views/response/show-response.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7x2 mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-x2">
+                    @include('employee.partials.employee-list')
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-x2">
+                    @include('freelancer.partials.freelancer-list')
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-x2">
+                    @include('vehicle.partials.vehicle-list')
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</x-app-layout>
