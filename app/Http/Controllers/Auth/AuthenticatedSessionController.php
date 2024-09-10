4de8dbbcbcde768609fd16ffbdbcc9985c592f1b
Leonardo-Lucas-DEVOPS <Leonardolucas181204@gmail.com>
2024-09-10 16:14:55 -0300

    - criação do metodo para atualização com registros com + 6 meses - criação de alerta de mudança de senha primeiro acesso - alerta para exclusão e alteração de senha de affiliados e admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -20,15 +20,34 @@ class AuthenticatedSessionController extends Controller
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
     public function store(LoginRequest $request): RedirectResponse
     {
+        // Autentica o usuário
         $request->authenticate();
 
+        // Regenera a sessão para evitar ataques de session fixation
         $request->session()->regenerate();
 
+        // Chama a função first_access após autenticação
+        return $this->first_access();
+    }
+
+    /**
+     * Verifica se o usuário precisa alterar a senha após verificação recente do e-mail.
+     */
+    public function first_access(): RedirectResponse
+    {
+        // Verifica se o usuário está autenticado e se o e-mail foi verificado
+        if (Auth::check() && Auth::user()->email_verified_at) {
+            // Verifica se a verificação de e-mail foi feita há menos de 5 minutos
+            if (Auth::user()->email_verified_at->diffInMinutes(now()) < 5) {
+                // Redireciona para a dashboard com uma mensagem de alerta
+                return redirect()->intended(route('dashboard', absolute: false))
+                                 ->with('alert', 'Por favor, altere sua senha em seu perfil.');
+            }
+        }
+
+        // Redireciona para a dashboard sem alerta caso a verificação seja maior que 5 minutos
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,17 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    Public function updateStatusForModel($model)
+{
+    // Busca todos os registros criados há mais de 3 meses
+    $records = $model::where('updated_at', '<=', now()->subMonths(6))
+                      ->where('return_status', '!=', 'Em Análise') // Evita alterar se já estiver "Em Análise"
+                      ->get();
+
+    // Atualiza o status de consulta para "Em Analise"
+    foreach ($records as $record) {
+        $record->return_status = 'Em Análise';
+        $record->save();
+    }
+}
 }
